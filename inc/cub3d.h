ac2e5a92d43be5266a8479d1814ac948495d89a8
Denice Montemayor <dmontema@1-C-27.42heilbronn.de>
2022-07-12 03:09:59 +0200

    added textures. added on map. added defines and structs in header file. first prototype for raycasting: followed guide.

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 17:04:31 by dmontema          #+#    #+#             */
-/*   Updated: 2022/07/09 23:14:44 by dmontema         ###   ########.fr       */
+/*   Updated: 2022/07/11 19:13:50 by dmontema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,59 @@
 
 # include <fcntl.h>
 # include <math.h>
+# include <stdbool.h>
 
 # define SUCC 1
 # define FAIL 0
 # define FILE_NOT_FOUND -1
 # define FILENAME_NOT_CORRECT -2
+
+# define WIDTH 1200
+# define HEIGHT 1080
+
+typedef struct	s_vect
+{
+	double	x;
+	double	y;
+}				t_vect;
+
+typedef struct	s_colour
+{
+	int	red;
+	int	green;
+	int	blue;
+}				t_colour;
+
+typedef struct	s_player
+{
+	t_vect	pos;
+	t_vect	dir;
+	t_vect	plane;
+}				t_player;
+
+typedef struct	s_data
+{
+	t_player	*player;
+	char		*file;
+	char		*map;
+	char		*n_texture;
+	char		*e_texture;
+	char		*s_texture;
+	char		*w_texture;
+	t_colour	c_colour;
+	t_colour	f_colour;
+}				s_data;
+
+typedef struct	s_parser_check
+{
+	bool	n_identifier;
+	bool	e_identifier;
+	bool	s_identifier;
+	bool	w_identifier;
+	bool	c_identifier;
+	bool	f_identifier;
+	bool	map_identifier;
+}				t_parser_check;
 
 int	file_parser(char *filename);
 
